b88dcc891594960cdeeeb762a08a2bbbe414c8fb
Kim Burrows <kim@shortroundgames.com>
2014-10-07 18:04:17 +0100

    - Switched over to our own Kibana 4 branch for testing.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default['kibana']['repo'] = "https://github.com/ShortRoundGames/kibana"
-default['kibana']['branch'] = "master"
+default['kibana']['branch'] = "kibana4"
 default['kibana']['git']['checkout'] = false
 default['kibana']['webserver'] = "nginx"
 default['kibana']['installdir'] = "/opt/kibana"
