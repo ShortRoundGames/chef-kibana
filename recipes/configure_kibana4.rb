a83fff516802935ff9e2f380d02744163e2c3501
Kim Burrows <kim@shortroundgames.com>
2014-10-13 16:32:39 +0100

    - Fixed re-running configure command.

--- recipes/configure_kibana4.rb
+++ recipes/configure_kibana4.rb
@@ -5,6 +5,7 @@ script "run_kibana" do
   user "root"
   cwd "/tmp"
   code <<-EOH
+    killall java
     chmod 0755 #{node['kibana']['installdir']}/current/server/bin/kibana.sh
     #{node['kibana']['installdir']}/current/server/bin/kibana.sh
   EOH
